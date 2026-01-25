5fcaab4e53f2ca9f323820cdcca98a6f7f16d548
lazKorsan <ahmeteldes2002@gmail.com>
2026-01-25 22:01:22 +0300

    pages classdan method kullanma eklendi

--- cypress89/cypress/e2e/ManuelTestSpecs/US045UrlUsageFromTestData.cy.js
+++ cypress89/cypress/e2e/ManuelTestSpecs/US045UrlUsageFromTestData.cy.js
@@ -1,18 +1,17 @@
 /// <reference types="cypress" />
-
 import { LoyalData } from '../../../TestData';
 import { loginToWebsite } from '../Methods/LoyalMethods';
+import LoyalHomePage from '../POM/LoyalHomePage.cy'; // Düzgün içe aktarma
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-
   return false;
 });
 
-describe('Test Data ile Url girisi', () => {
+describe('Test Data ile Url girişi', () => {
   it('US045 Test01', () => {
-    cy.visit(LoyalData.loyalUrl);
+    const loyalHomePage = new LoyalHomePage(); // Sınıfın örneğini oluştur
+    loyalHomePage.visitHomePage(); // Metodu çağır
     loginToWebsite();
-
-
   });
 });
+// 55:05
